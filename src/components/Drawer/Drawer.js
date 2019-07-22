28caa256a01c6ba7f1d4b756db84aad56856b1fd
dontry <mccoy018@gmail.com>
2019-07-22 23:17:41 +1000

    feat(drawer): add Drawer component with test cases

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes  from "prop-types";
+import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
 import { Typography } from "@material-ui/core";
@@ -21,23 +21,98 @@ const useStyles = makeStyles({
 });
 
 const ServiceDataType = {
+  parent: PropTypes.string,
+  made_sla: PropTypes.string,
+  caused_by: PropTypes.string,
+  watch_list: PropTypes.string,
+  upon_reject: PropTypes.string,
+  child_incidents: PropTypes.string,
+  hold_reason: PropTypes.string,
+  approval_history: PropTypes.string,
+  resolved_by: PropTypes.string,
+  sys_updated_by: PropTypes.string,
+  opened_by: PropTypes.string,
+  user_input: PropTypes.string,
+  sys_created_on: PropTypes.string,
+  sys_domain: PropTypes.string,
+  state: PropTypes.string,
+  sys_created_by: PropTypes.string,
+  knowledge: PropTypes.string,
+  order: PropTypes.string,
+  calendar_stc: PropTypes.string,
+  closed_at: PropTypes.string,
+  cmdb_ci: PropTypes.string,
+  delivery_plan: PropTypes.string,
+  impact: PropTypes.string,
+  active: PropTypes.string,
+  work_notes_list: PropTypes.string,
+  business_service: PropTypes.string,
+  priority: PropTypes.string,
+  sys_domain_path: PropTypes.string,
+  rfc: PropTypes.string,
+  time_worked: PropTypes.string,
+  expected_start: PropTypes.string,
+  opened_at: PropTypes.string,
+  business_duration: PropTypes.string,
+  group_list: PropTypes.string,
+  work_end: PropTypes.string,
   caller_id: PropTypes.string,
+  reopened_time: PropTypes.string,
   resolved_at: PropTypes.string,
   approval_set: PropTypes.string,
-  subcategory: PropTypes.string
+  subcategory: PropTypes.string,
+  work_notes: PropTypes.string,
+  close_code: PropTypes.string,
+  correlation_display: PropTypes.string,
+  delivery_task: PropTypes.string,
+  work_start: PropTypes.string,
+  additional_assignee_list: PropTypes.string,
+  business_stc: PropTypes.string,
+  description: PropTypes.string,
+  calendar_duration: PropTypes.string,
+  close_notes: PropTypes.string,
+  notify: PropTypes.string,
+  sys_class_name: PropTypes.string,
+  closed_by: PropTypes.string,
+  follow_up: PropTypes.string,
+  parent_incident: PropTypes.string,
+  contact_type: PropTypes.string,
+  reopened_by: PropTypes.string,
+  urgency: PropTypes.string,
+  problem_id: PropTypes.string,
+  company: PropTypes.string,
+  reassignment_count: PropTypes.string,
+  activity_due: PropTypes.string,
+  severity: PropTypes.string,
+  comments: PropTypes.string,
+  approval: PropTypes.string,
+  sla_due: PropTypes.string,
+  comments_and_work_notes: PropTypes.string,
+  due_date: PropTypes.string,
+  sys_mod_count: PropTypes.string,
+  reopen_count: PropTypes.string,
+  sys_tags: PropTypes.string,
+  escalation: PropTypes.string,
+  upon_approval: PropTypes.string,
+  location: PropTypes.string,
+  category: PropTypes.string
 };
 
 const Drawer = ({ isOpen, close, open, number, data }) => {
-
-  const  handleOpen = () => {
+  const handleOpen = () => {
     open();
-  }
+  };
   const handleClose = () => {
     close();
   };
 
   return (
-    <SwipeableDrawer anchor="right" open={isOpen} onOpen={handleOpen} onClose={handleClose}>
+    <SwipeableDrawer
+      anchor="right"
+      open={isOpen}
+      onOpen={handleOpen}
+      onClose={handleClose}
+    >
       <DrawerContent number={number} data={data} />
     </SwipeableDrawer>
   );
@@ -65,7 +140,7 @@ export const DrawerContent = ({ number, data }) => {
 DrawerContent.propTypes = {
   number: PropTypes.string.isRequired,
   data: PropTypes.shape(ServiceDataType)
-}
+};
 
 export const InfoItem = ({ label, value = "" }) => {
   const classes = useStyles();
@@ -80,6 +155,6 @@ export const InfoItem = ({ label, value = "" }) => {
 InfoItem.propTypes = {
   label: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired
-}
+};
 
 export default Drawer;
--- src/components/Drawer/Drawer.test.js
+++ src/components/Drawer/Drawer.test.js
@@ -1,31 +1,109 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import Drawer, {DrawerContent} from "./Drawer";
+import Drawer, { DrawerContent } from "./Drawer";
 
 const number = "INCXXXX";
 
 const serviceData = {
- caller_id: "",
-    resolved_at: "",
-    approval_set: "",
-    subcategory: "",
-}
+  parent: "",
+  made_sla: "false",
+  caused_by: "",
+  watch_list: "",
+  upon_reject: "null",
+  child_incidents: "",
+  hold_reason: "",
+  approval_history: "",
+  resolved_by: "System Administrator",
+  sys_updated_by: "system",
+  opened_by: "System Administrator",
+  user_input: "",
+  sys_created_on: "2017-07-10 15:14:01",
+  sys_domain: "global",
+  state: "Closed",
+  sys_created_by: "admin",
+  knowledge: "false",
+  order: "",
+  calendar_stc: "7,690,046",
+  closed_at: "2019-05-27 12:56:17",
+  cmdb_ci: "ApplicationServerPeopleSoft",
+  delivery_plan: "",
+  impact: "1 - High",
+  active: "false",
+  work_notes_list: "",
+  business_service: "",
+  priority: "1 - Critical",
+  sys_domain_path: "/",
+  rfc: "",
+  time_worked: "",
+  expected_start: "",
+  opened_at: "2018-12-01 15:06:52",
+  business_duration: "21 Days 15 Hours 14 Minutes",
+  group_list: "",
+  work_end: "",
+  caller_id: "Alejandro Mascall",
+  reopened_time: "",
+  resolved_at: "2019-02-28 15:14:18",
+  approval_set: "",
+  subcategory: "null",
+  work_notes:
+    "2019-02-28 13:17:01 - System Administrator (Work notes)\ntest\n\n",
+  close_code: "Closed/Resolved by Caller",
+  correlation_display: "",
+  delivery_task: "",
+  work_start: "",
+  additional_assignee_list: "",
+  business_stc: "1,869,258",
+  description:
+    "Watcher daemon detected that the CPU was 100% busy for more than 10 minutes",
+  calendar_duration: "89 Days 7 Minutes",
+  close_notes: "Fixed",
+  notify: "Do Not Notify",
+  sys_class_name: "Incident",
+  closed_by: "System Administrator",
+  follow_up: "",
+  parent_incident: "",
+  contact_type: "Email",
+  reopened_by: "",
+  urgency: "1 - High",
+  problem_id: "",
+  company: "ACME Germany",
+  reassignment_count: "1",
+  activity_due: "2019-02-28 15:17:01",
+  severity: "1 - High",
+  comments: "",
+  approval: "null",
+  sla_due: "UNKNOWN",
+  comments_and_work_notes:
+    "2019-02-28 13:17:01 - System Administrator (Work notes)\ntest\n\n",
+  due_date: "",
+  sys_mod_count: "15",
+  reopen_count: "",
+  sys_tags: "",
+  escalation: "Overdue",
+  upon_approval: "null",
+  location: "Bockenheimer Landstraße 223, Frankfurt",
+  category: "Hardware"
+};
 
 describe("Drawer Component", () => {
   it("renders Drawer with service data", () => {
     const close = jest.fn();
-    const {container} = render(<Drawer isOpen={true} close={close} number={number} data={serviceData}/>)
+    const { container } = render(
+      <Drawer isOpen={true} close={close} number={number} data={serviceData} />
+    );
     expect(container.firstChild).toMatchSnapshot();
-  })
-})
+  });
+});
 
 describe("DrawerContent Component", () => {
   it("renders Drawer with service data", () => {
-    const {container, queryByText} = render(<DrawerContent isOpen={true}  number={number} data={serviceData}/>)
+    const { container, queryByText } = render(
+      <DrawerContent isOpen={true} number={number} data={serviceData} />
+    );
     expect(queryByText(number)).toBeTruthy();
-    Object.keys(serviceData).map(key => {
+    Object.keys(serviceData).forEach(key => {
       expect(queryByText(key)).toBeTruthy();
-    })
+    });
     expect(container.firstChild).toMatchSnapshot();
-})
-})
+  });
+});
